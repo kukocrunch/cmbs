36ee163dfba8ebb82279368688eeec4deadd9fbd
Jaycee Rolluqui <jcrolluqui@gmail.com>
2016-11-27 00:04:59 +0800

    added api and affected models

--- index.php
+++ index.php
@@ -38,10 +38,16 @@ if( isset($_GET['class']) && $_GET['class'] !='' ){
 
 			foreach($uriArray as $k => $v){
 				foreach($_GET as $key => $var){
-					if($v != $var){
+					if($key == 'class' && $v != $var){
 						$vars[] = $v;
 					}
 				}
+			}
+			if(isset($vars[0]) && $vars[0] == "f"){
+				$function = trim($vars[1]);
+				unset($vars[0]);
+				unset($vars[1]);
+				$vars = array_values($vars);
 			}
 			try{
 				if(is_callable(array("Controller\\".trim($_GET['class']),$function))) { 

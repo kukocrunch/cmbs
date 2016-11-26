cab1c64e897568edfa0d398966951410c3e1703c
Benjo <benjo.ang@gmail.com>
2016-11-27 06:50:25 +0800

    Commented ComposerAutoloader to prevent error 500s

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1a58a52a84401bd5a4e79430527f722d::getLoader();
+//return ComposerAutoloaderInit1a58a52a84401bd5a4e79430527f722d::getLoader();

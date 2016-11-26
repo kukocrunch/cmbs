ced779984d6b19cf9605eef0bb82bd423cf2c71b
Jaycee Rolluqui <jcrolluqui@gmail.com>
2016-11-27 01:08:06 +0800

    modified send() variables

--- controller/api.php
+++ controller/api.php
@@ -50,7 +50,7 @@ class api extends base {
             $account = $accountDAO->getAccount($accountNo, $mobileNo);
             $terminal = $terminalDAO->getTerminal($terminalNo);
             if(!empty($account) && !empty($terminal)){
-                $vars['otp'] = "success";
+                $vars['type'] = "success";
                 $vars['mobile_number'] = $mobileNo;
                 $vars['terminalNo'] = $terminalNo;
                 $this->send($vars);
@@ -65,11 +65,11 @@ class api extends base {
     public function send( $vars ){
         // api/send/otp/$mobilenumber
         // api/check/$accountid/$accountnumber/encrypted($terminal)/
-        $otp = $vars['otp'];
+        $type = $vars['type'];
         $config = new Config();
         Nonce::generate();
         $url = $config->chikka;
-        $randpin = 0;
+        $otp = 0;
         $params = [
             "message_type" => "SEND",
             "mobile_number" => $vars['mobile_number'],
@@ -78,13 +78,16 @@ class api extends base {
             "client_id" => $config->chikka_ci,
             "secret_key" => $config->chikka_sk
         ];
-        switch($otp){
+        switch($type){
             case "success":
                 $randpin = rand(100000,999999);
                 $params['message'] = "Thank you for choosing CMBS. Your one time use pin is: ".$randpin;
                 break;
             case "fail":
                 $params['message'] = "Thank you for choosing CMBS. Unfortunately your account does not exist. Please try again.";
+                break;
+            case "notif":
+                $params['message'] = "Your transaction has been complete.";
                 break;
             default:
                 print_r(json_encode(array("code"=>"400","message"=>"Bad Request")));
@@ -122,81 +125,5 @@ class api extends base {
         }
 
     }
-
-
-
-    private function sendTransfer( $vars ){
-        $curl = curl_init();
-        $config = new Config;
-        $channel_id = "UHACK_0028";
-        extract( $this->load->model( "transaction" ) );
-        extract( $this->load->model( "account" ) );
-        $account_id_from = $_SESSION['account_id'];
-        $account_id_to = $accountDAO->getByMobileNo($vars['mobile_no'])->id;
-        $transaction->type = "stok";
-        $transaction->dest_mobile = $vars['mobile_no'];
-        $transaction->source_mobile = $accountDAO->getByAccountId($account_id_from)->mobile_no;
-        $transaction->amount = $vars['amount'];
-        // var_dump($transaction);
-        $transaction_id = $transactionDAO->save($transaction, true);
-        
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.us.apiconnect.ibmcloud.com/ubpapi-dev/sb/api/RESTs/transfer",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_SSL_VERIFYHOST => 0,
-          CURLOPT_SSL_VERIFYPEER => 0,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => "{\"channel_id\":\"".$channel_id."\",\"transaction_id\":\"".$transaction_id."\",\"source_account\":\"".$_SESSION['account_id']."\",\"source_currency\":\"php\",\"target_account\":\"".$account_id_to."\",\"target_currency\":\"php\",\"amount\":".$vars['amount']."}",
-          CURLOPT_HTTPHEADER => array(
-            "accept: application/json",
-            "content-type: application/json",
-            "x-ibm-client-id: bfac49db-0569-412d-925b-263b3e640c4c",
-            "x-ibm-client-secret: uU0vR1hC1bT6xI3cP1jD3uI1jW6cK0nG0iS4fT6vO8mL2xL6kJ"
-          ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-          echo "cURL Error #:" . $err;
-        } else {
-          $response = json_decode($response);
-          $transaction->id = "";
-          if( $response->status != "S" || $response->error_message != ""){
-            // header("Location:".$config->http."funds/transfer/?error=".$response->error_message);
-          } else{
-                $transaction->reference = $response->confirmation_no;
-                
-                $update = $transactionDAO->updateTransaction( $transaction, $transaction_id );
-                $transaction = $transactionDAO->getTransactionById($transaction_id);
-                
-                Nonce::refresh();
-                $config = new Config;
-                $vars['title']  = "Funds - Transfer";
-                $vars['config'] = $config;
-                $vars['nonce'] = Nonce::generate();
-                $vars['account'] = $accountDAO->getByMobileNo($vars['mobile_no']);
-                echo $this->load->view( 'funds.transfer', $vars );  
-          }
-          
-        }
-    }
-
-    public function generate( $vars ){
-        //
-    }
-
-    public function transact( $vars ){
-        // api/transact/{withdraw,deposit,pay}/accountid
-    }
-
-
 
 }
